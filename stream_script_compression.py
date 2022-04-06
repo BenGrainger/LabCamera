54d99749679a57c795dca7645f77b1f3122704e8
Ben G <bfgrainger@gmail.com>
2022-04-06 16:34:25 +0100

    c

--- stream_script_compression.py
+++ stream_script_compression.py
@@ -5,10 +5,12 @@ suffix = '_264.avi'
 folder = r'C:\Users\BMLab21\Documents\CrabStreams'
 contents = os.listdir(folder)
 for file_name in contents:
-    if '.avi' in file_name and suffix not in file_name:
-        input_name = file_name
-        output_name = os.join(folder, file_name[:-4] + suffix)
-        command = 'ffmpeg -i {} -c:v libx264 -crf 21 vf- format=yuv420p {}'.format(input_name, output_name)
-        result = subprocess.run(command)
-        pathlib.Path(json_server_file_path).write_text(json.dumps(metaData))
-        print('compression completed')
+    if '.avi' in file_name:
+        output_name = file_name[:-4] + suffix
+        if len(output_name) == len('YYYY-MM-DD_265.avi') and output_name not in contents:
+            input_path = os.path.join(folder, file_name)
+            output_path = os.path.join(folder, output_name)
+            command = 'ffmpeg -i {} -c:v libx264 -crf 21 vf- format=yuv420p {}'.format(input_path, output_path)
+            print(command)
+            result = subprocess.run(command)
+            print('compression completed')
