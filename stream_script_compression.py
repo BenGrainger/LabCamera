0cc527167b7cd376bb6491f39c8023e2d9c37986
Ben G <bfgrainger@gmail.com>
2022-04-06 16:10:11 +0100

    commit

--- stream_script_compression.py
+++ stream_script_compression.py
@@ -8,7 +8,7 @@ for file_name in contents:
     if '.avi' in file_name and suffix not in file_name:
         input_name = file_name
         output_name = os.join(folder, file_name[:-4] + suffix)
-        command = 'ffmpeg -i {} -c:v libx265 -crf 29 {}'.format(input_name, output_name)
+        command = 'ffmpeg -i {} -c:v libx264 -crf 21 vf- format=yuv420p {}'.format(input_name, output_name)
         result = subprocess.run(command)
         pathlib.Path(json_server_file_path).write_text(json.dumps(metaData))
         print('compression completed')
